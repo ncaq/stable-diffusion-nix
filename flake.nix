e229afc9709d6a8b22b254b60957a498e17d185b
ncaq <ncaq@ncaq.net>
2025-08-16 13:29:46 +0900

    feat: add initial flake.nix with treefmt and devShell setup
    - プロジェクトの初期flake.nixを追加
    - treefmt-nixによるフォーマット設定を導入
    - devShells.defaultを追加
    - nixpkgs安定版とunstableを入力に設定
    - cachixのバイナリキャッシュを利用する設定を追加

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "stable-diffusion to managed by Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      treefmt-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          config,
+          ...
+        }:
+        {
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              nixfmt.enable = true;
+              prettier.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+            };
+          };
+          devShells.default = pkgs.mkShell { };
+        };
+    };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
