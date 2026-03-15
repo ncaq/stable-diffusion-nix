b9705876bbb30dfb1582c803b81e65a383ab8ccd
ncaq <ncaq@ncaq.net>
2026-03-15 14:44:09 +0900

    ci: GitHub Actionsのキャッシュ設定を調整
    Cachixはなるべく統一してncaqを使用するように変更したため。
    また統一して自前のキャッシュサーバを使用します。

--- flake.nix
+++ flake.nix
@@ -474,15 +474,17 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
+      "https://niks3-public.ncaq.net/"
+      "https://ncaq.cachix.org/"
+      "https://nix-community.cachix.org/"
       "https://cuda-maintainers.cachix.org"
-      "https://stable-diffusion-nix.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "niks3-public.ncaq.net-1:e/B9GomqDchMBmx3IW/TMQDF8sjUCQzEofKhpehXl04="
+      "ncaq.cachix.org-1:XF346GXI2n77SB5Yzqwhdfo7r0nFcZBaHsiiMOEljiE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "stable-diffusion-nix.cachix.org-1:/gQjQ/1RF50Ko/SvuoFwc5Y4+UZepTSvbySXGgwyJWo="
     ];
   };
 }
